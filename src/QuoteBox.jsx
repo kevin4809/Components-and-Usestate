88e8af2f1f90d0d20c813ee7473ce7ec0206cd20
kevin4809 <kevinedk33@gmail.com>
2022-04-30 07:12:56 -0500

    1
    First version

---
+++ src/QuoteBox.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import phrases from './quotes.json'
+import './Style.css'
+const QuoteBox = () => {
+
+  console.log(phrases)
+  const [count, setCount] = useState(0);
+  const changeColors = ["lightgreen", "lightcoral", "lightblue", "aquamarine", "lightgray", "lightsalmon", "lightseagreen", "chocolate", "lightsteelblue"]
+
+  const next = () => {
+    let random = Math.round(Math.random() * phrases.length);
+    setCount(random)
+  }
+
+  let randomColors = Math.round(Math.random() * changeColors.length);
+
+  document.body.style = `background: ${changeColors[randomColors]}`
+
+
+  return (
+    <div className='container' style={{ color: changeColors[randomColors] }}>
+      <div className="card">
+        <i className="fa-solid fa-quote-left com"></i>
+        <h1>{phrases[count].quote}</h1>
+        <h2>{phrases[count].author}</h2>
+        <div className="button">
+          <button className="fa-solid fa-angle-right" onClick={next} style={{ background: changeColors[randomColors] }}></button>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default QuoteBox
